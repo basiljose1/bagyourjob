14bf2259a0ae125fc03f9105cdd9a1b57913711b
Basil Jose <basiljose1@gmail.com>
2017-02-07 15:53:15 +0530

    First commit

---
+++ app/src/main/java/com/bagyourjob/bagyourjob/HomeActivity.java
@@ -0,0 +1,59 @@
+package com.bagyourjob.bagyourjob;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_home);
+
+
+        myWebView = (WebView) findViewById(R.id.myWebView);
+        myWebView.loadUrl("https://m.bagyourjob.com");
+        myWebView.setWebViewClient(new HomeActivity.MyWebViewClient());
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if (Uri.parse(url).getHost().equals("www.centerend.com")) {
+                return false;
+
+            }
+
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+            return true;
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && myWebView.canGoBack()) {
+            myWebView.goBack();
+            return true;
+        }
+        else
+        {
+            finish();
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
